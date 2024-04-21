5c0f798d3c3f8d7f9de52e5aa58e7fac42b0e31c
surya <surya59g@gmail.com>
2024-04-22 00:51:35 +0530

    Array4:  2D array

--- src/main/java/org/array/Array4.java
+++ src/main/java/org/array/Array4.java
@@ -7,7 +7,7 @@ public class Array4 {
 //printing 2D array
             for(int i=0;i<3;i++){
                 for(int j=0;j<3;j++){
-                    System.out.print(arr[i][j]+" ");
+                    System.out.print(arr[i][j]+" hii");
                 }
                 System.out.println();
             }
