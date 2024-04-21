d42b033a1107cde98ee1f305789bb877f1bb028d
surya <surya59g@gmail.com>
2024-04-22 00:43:18 +0530

    Array4:  2D array

--- src/main/java/org/array/App.java
+++ src/main/java/org/array/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Array1: Array Declaration, initialize and print. " );
         System.out.println( " Array2: Scanner method " );
-        System.out.println( " Array2: for each method" );
+        System.out.println( " Array3: for each method" );
     }
 }
