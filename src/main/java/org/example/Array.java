cfd75e47ed3bfda553f5c0873f81c11b22db0685
surya <surya59g@gmail.com>
2024-04-21 22:44:24 +0530

    Array example1

---
+++ src/main/java/org/example/Array.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Array {
+    public static void main(String[] args) {
+
+//        int marks[] = new int[3];
+//        marks[0] = 97;
+//        marks[1] = 70;
+//        marks[2] = 80;
+
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+
+
+        int marks[] = {97,98,56};
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
