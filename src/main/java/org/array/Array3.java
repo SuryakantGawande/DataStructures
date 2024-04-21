df02180774c69d8a22e8a4a99be1606e5faa5a47
surya <surya59g@gmail.com>
2024-04-22 00:38:35 +0530

    Array3: for each method

---
+++ src/main/java/org/array/Array3.java
@@ -0,0 +1,10 @@
+package org.array;
+
+public class Array3 {
+    //Java Program to print the array elements using for-each loop
+        public static void main(String args[]){
+            int arr[]={33,3,4,5};
+//printing array using for-each loop
+            for(int i:arr)
+                System.out.println(i);
+        }}
