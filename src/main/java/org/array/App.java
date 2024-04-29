0988198036397c2e13b804988eaee16f187b7c3b
surya <surya59g@gmail.com>
2024-04-29 14:36:46 +0530

    4

--- src/main/java/org/array/App.java
+++ src/main/java/org/array/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Array1: Array Declaration, initialize and print. " );
         System.out.println( " Array2: Scanner method " );
         System.out.println( " Array3: for each method" );
+        System.out.println( " Array3: "" " );
     }
 }
--- src/main/java/org/array/Array1.java
+++ src/main/java/org/array/Array1.java
@@ -1,14 +1,16 @@
 package org.array;
 
 public class Array1 {
-    public static void main(String args[]){
-        int a[]=new int[5];//declaration and instantiation
-        a[0]=10;//initialization
-        a[1]=20;
-        a[2]=70;
-        a[3]=40;
-        a[4]=50;
-//traversing array
-        for(int i=0;i<a.length;i++)
-            System.out.println(a[i]);
-    }}
+    public static void main(String args[]) {
+        int a[] = new int[5];//declaration and instantiation
+        a[0] = 10;//initialization
+        a[1] = 20;
+        a[2] = 70;
+        a[3] = 40;
+        a[4] = 50;
+//traversing array     //  Line format Ctl+alt+L
+        for (int j : a) System.out.println(j);
+        System.out.println("done");
+        System.out.println("done 2");
+    }
+}
